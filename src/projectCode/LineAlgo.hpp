388b2e70be6d67604d9219bf2afb726f54d1b74e
Steven Weller <skwiglyman@gmail.com>
2022-11-29 17:32:50 -0500

    Added Brensenham Line Algorithm

--- src/projectCode/LineAlgo.hpp
+++ src/projectCode/LineAlgo.hpp
@@ -1,2 +1,10 @@
 #pragma once
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include <vector>
+
+// This file is a header file that connects all of the line drawing functions, and filling algorithms to the main program
+
+std::vector<glm::vec2> DrawLine(glm::vec2 p1, glm::vec2 p2);
