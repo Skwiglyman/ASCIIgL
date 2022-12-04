6c157e950bd24b5fd41647ebe4831ea3eba8d8a2
Steven Weller <skwiglyman@gmail.com>
2022-12-03 22:03:27 -0500

    Fixed Draw Line

--- src/projectCode/LineAlgo.hpp
+++ src/projectCode/LineAlgo.hpp
@@ -7,4 +7,4 @@
 
 // This file is a header file that connects all of the line drawing functions, and filling algorithms to the main program
 
-std::vector<glm::vec2> DrawLine(glm::vec2 p1, glm::vec2 p2);
+std::vector<glm::vec2> DrawLine(int x1, int y1, int x2, int y2);
